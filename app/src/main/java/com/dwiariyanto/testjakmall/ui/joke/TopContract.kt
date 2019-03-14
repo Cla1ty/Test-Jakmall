8d8789a4b45bd18c544e25564be07eee228ec92a
Dwi Ariyanto <creatures99@gmail.com>
2019-03-14 19:57:06 +0700

    refactor: Change top contract

--- app/src/main/java/com/dwiariyanto/testjakmall/ui/joke/TopContract.kt
+++ app/src/main/java/com/dwiariyanto/testjakmall/ui/joke/TopContract.kt
@@ -1,20 +1,19 @@
 package com.dwiariyanto.testjakmall.ui.joke
 
 import com.dwiariyanto.testjakmall.base.mvp.BaseLoadDataView
-import com.dwiariyanto.testjakmall.data.model.joke.JokeData
 
 /**
  * Created by Dwi Ariyanto on 14/03/19.
  */
 
-object JokeContract {
-    interface View : BaseLoadDataView<List<JokeData>>{
+object TopContract {
+    interface View : BaseLoadDataView<List<Any>>{
         fun showHelloDialog()
     }
 
     interface Presenter {
         fun loadRandomJoke()
-        fun moveJokeToTopList(jokeData: JokeData)
+        fun moveJokeToTopList(id:Int)
         fun selectJoke()
         fun addJoke()
     }
